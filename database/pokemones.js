01313770e67f54a3d321516a3fbb1cf6c3d06ae4
JuanCamilo <camiloooo1414v@gmail.com>
2025-05-20 07:01:34 -0500

    database

--- database/pokemones.js
+++ database/pokemones.js
@@ -4,7 +4,6 @@ const pokemones = [
   new Pokemon(1, 'Bulbasaur', 0.7, 'Espesura', 'https://pokenea-imagenes14.s3.us-east-1.amazonaws.com/sapo_verde.png', 'La paciencia es la clave para florecer.'),
   new Pokemon(4, 'Charmander', 0.6, 'Mar Llamas', 'https://pokenea-imagenes14.s3.us-east-1.amazonaws.com/Flamariete.png', 'El fuego interior nunca debe apagarse.'),
   new Pokemon(7, 'Squirtle', 0.5, 'Torrente', 'https://pokenea-imagenes14.s3.us-east-1.amazonaws.com/tortuga_azul.png', 'La flexibilidad te permite superar cualquier obstáculo.'),
-  
   new Pokemon(25, 'Pikachu', 0.4, 'Electricidad Estática', 'https://pokenea-imagenes14.s3.us-east-1.amazonaws.com/Electrofuria.png', 'La energía positiva ilumina el camino.'),
   new Pokemon(133, 'Eevee', 0.3, 'Adaptación', 'https://pokenea-imagenes14.s3.us-east-1.amazonaws.com/okidogi.png', 'La versatilidad es el camino a la evolución.'),
   new Pokemon(150, 'Mewtwo', 2.0, 'Presión', 'https://pokenea-imagenes14.s3.us-east-1.amazonaws.com/mariposa_morada.png', 'El verdadero poder viene del autoconocimiento.'),
